cebb945b19e0017b88c420c959a78cf447311ec3
Gasskin <415753928@qq.com>
2024-05-09 22:18:55 +0800

    表格改造

---
+++ Server/Server/Core/Core/World/Module/Config/ConfigLoader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Luban;
+#if DOTNET || UNITY_STANDALONE
+using System.Threading.Tasks;
+#endif
+
+namespace ET
+{
+    public class ConfigLoader: Singleton<ConfigLoader>, ISingletonAwake
+    {
+        public struct GetAllConfigBytes
+        {
+        }
+
+        public struct GetOneConfigBytes
+        {
+            public string ConfigName;
+        }
+
+        public void Awake()
+        {
+        }
+
+        public async ETTask Reload()
+        {
+            this.LoadAsync().Coroutine();
+            await ETTask.CompletedTask;
+        }
+
+        public async ETTask LoadAsync()
+        {
+            await EventSystem.Instance.Invoke<GetAllConfigBytes, ETTask>(new GetAllConfigBytes());
+        }
+    }
+}
