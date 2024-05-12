8b81758b2b7f2a2e0ec7a65e0b879d01d1f660a5
Gasskin <415753928@qq.com>
2024-05-12 19:47:59 +0800

    缓存服

---
+++ Server/Server/Hotfix/Demo/UnitCache/FiberInit_UnitCache.cs
@@ -0,0 +1,10 @@
+﻿namespace ET.Server;
+
+[Invoke((long)SceneType.UnitCache)]
+public class FiberInit_UnitCache:AInvokeHandler<FiberInit, ETTask>
+{
+    public override async ETTask Handle(FiberInit args)
+    {
+        await ETTask.CompletedTask;
+    }
+}
